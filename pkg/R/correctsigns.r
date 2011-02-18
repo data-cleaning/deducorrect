e5aed834df2ba9b258f302c43ef7f60aaf4df1e2
mark.vanderloo <mark.vanderloo@0f91cfca-1d3f-6790-2ae6-82cf2eacda8d>
2011-02-18 13:12:04 +0000

    added variable fixing to correctSigns

--- pkg/R/correctsigns.r
+++ pkg/R/correctsigns.r
@@ -8,27 +8,26 @@
 #'
 #' @param A The \code{matrix} part of an \code{editmatrix}
 #' @param r A numerical record.
-#' @param inViolatedEdits An integer vector containing indices in the columns 
-#'      of \code{A}, alocating the variables which occur in the edits violated 
-#'      by \code{r}
+#' @param adapt A logical vector of length \code{ncol(A)} indicating which variables may be changed.
 #' @param maxSigns how many signs may maximally be flipped? 
 #' @param eps Tolerance for equality restriction checking
 #' 
 #' @return A \code{list} with vectors of length \code{r} with coefficients 
 #'      in \eqn{\{-1,1\}}. Empty \code{list} if no solution is found.
 #'
-getSignCorrection <- function(A, r, inViolatedEdits, maxSigns, eps){
+getSignCorrection <- function(A, r, adapt, maxSigns, eps){
 
     v <- rep(1,nrow(r))
     if (all(abs(A%*%r) <= eps)) return(list(v))
-    nViolated <- length(inViolatedEdits)
+    adapt <- which(adapt)
+    nViolated <- length(adapt)
     S <- list()
     j <- 1
     for ( nsigns in 1:min(maxSigns, max(nViolated%/%2,1)) ){
         I <- combn(1:nViolated, nsigns)
         for ( i in 1:ncol(I) ){
            s <- v
-           s[inViolatedEdits[I[, i]]] <- -1
+           s[adapt[I[, i]]] <- -1
            if ( all(abs(A %*% (r*s)) <= eps) ){
                 S[[j]] <- s
                 j <- j+1
@@ -39,14 +38,30 @@ getSignCorrection <- function(A, r, inViolatedEdits, maxSigns, eps){
     return(S)
 }
 
+#' Try to resolve linear edits by adapting signs
+#'
+#' 
+#' @param E An object of class \code{editmatrix} 
+#' @param dat The data to correct
+#' @param maxSigns Maximum number of signs that may be changed. Defaults to \eqn{ncol(E)%/%2}.
+#' @param eps Tolerance on edit check. Defaults to \code{sqrt(.Machine.double.eps)}
+#' @param weight Positive numeric vector of length ncol(E). Variables with heigher 
+#'      reliability weight are less likely to be changed. Defaults to \code{rep(1,ncol(E))}
+#' @param fix character vector. Names of variables which may not be changed.
+#' @export
 correctSigns <- function(
     E, 
     dat,
     maxSigns = ncol(E)%/%2,
     eps=sqrt(.Machine$double.eps),
-    weight = rep(1,ncol(E))
-    ){
-# TODO swapping and fix variables    
+    weight = rep(1,ncol(E)),
+    fix = NA ){
+
+    notFixed <- rep(TRUE,ncol(E))
+    if ( !identical(fix, NA) ){
+        notFixed <- !(colnames(E) %in% fix)
+    }
+
     cn <- colnames(E)
     D <- as.matrix(dat[ ,cn])
     A <- as.matrix(E)
@@ -55,8 +70,8 @@ correctSigns <- function(
     for ( i in 1:nrow(dat) ){
         r <- t(D[i,,drop=FALSE])
         violated <- abs(A %*% r) > eps            
-        inViolatedEdits <- which(colSums(abs(E[violated, ,drop=FALSE])) > 0)
-        S <- getSignCorrection(A, r, inViolatedEdits, maxSigns, eps)
+        adapt <- notFixed & colSums(abs(E[violated, ,drop=FALSE])) > 0
+        S <- getSignCorrection(A, r, adapt, maxSigns, eps)
         if ( length(S) > 0 ){
             wvec <- w %*% (sapply(S,matrix)<0)
             s <- S[[which.max(wvec)]]

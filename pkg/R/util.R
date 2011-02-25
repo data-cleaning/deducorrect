2caaaca3a9db2d6795a4aca6e42b6aac9d022bbc
mark.vanderloo <mark.vanderloo@0f91cfca-1d3f-6790-2ae6-82cf2eacda8d>
2011-02-25 07:46:55 +0000

    removed <NA> as level from ordered factor since ordering doesn't work as expected

--- pkg/R/util.R
+++ pkg/R/util.R
@@ -6,15 +6,15 @@
 #' The status vector is an \code{ordered} factor with levels
 #'
 #' \tabular{ll}{
-#'  \code{NA} \tab  non-validity of the record could not be established\cr
-#'  \code{invalid} \tab record is invalid but could not be corrected\cr
-#'  \code{partially} \tab record could not be corrected to satisfy all edit restrictions\cr
-#'  \code{corrected}\tab record satisfies all edit restrictions after correction\cr
-#'  \code{valid} \tab record violates no edit restrictions\cr
+#'  \code{NA}           \tab  non-validity of the record could not be established\cr
+#'  \code{invalid}      \tab record is invalid but could not be corrected\cr
+#'  \code{partially}    \tab record violates less edits then before entering the function\cr
+#'  \code{corrected}    \tab record satisfies all edit restrictions after correction\cr
+#'  \code{valid}        \tab record violates no edit restrictions\cr
 #' }
-#' where \code{NA < invalid < partially < corrected < valid}
+#' where \code{invalid < partially < corrected < valid}
 #' 
-#' Note that \code{<NA>} is an actual level here.
+
 #'
 #' This function is \code{deducorrect} internal.
 #'
@@ -26,13 +26,11 @@
 #' @example examples/status.R
 #'
 status <- function(n, ini=NA){
-    st <- c(NA, "invalid", "partially", "corrected", "valid")
+    st <- c("invalid", "partially", "corrected", "valid")
     if ( ! ini %in% st ){
         stop(paste("invalid status level, allowed are NA, ",paste(st,collapse=", ")))
     }
-    statvec <- ordered(rep(ini,n), levels=c("NAPLACEHOLDER", st))
-    attr(statvec,"levels") <- st
-    return(statvec)
+    return(ordered(rep(ini,n), levels=st))
 }
 
 

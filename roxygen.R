b12bae2ae3854210beee597b4d5d77a53d3149c9
Edwin de Jonge <edwindjonge@gmail.com>
2011-09-26 21:38:22 +0200

    changed into roxygen2

--- roxygen.R
+++ roxygen.R
@@ -1,14 +1,13 @@
-library(roxygen)
+library(roxygen2)
+options(error=traceback)
 unlink( 'pkg/man', TRUE)
 
-setwd("pkg")
-
+setwd('pkg')
 roxygenize( '.'
           , roxygen.dir='.'
           , copy.package=FALSE
           , unlink.target=TRUE
-          , use.Rd2 = TRUE
-		    )
+  	    )
 
 if (length(list.files('inst/doc')) == 0){
    unlink( 'inst/doc', TRUE)   

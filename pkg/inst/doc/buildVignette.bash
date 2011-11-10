b558bb61e8ebd8b20374045c5c90e279cc1e39f4
Mark van der Loo <mark.vanderloo@gmail.com>
2011-11-10 11:12:48 +0100

    split vignette in two

--- pkg/inst/doc/buildVignette.bash
+++ pkg/inst/doc/buildVignette.bash
@@ -1,8 +1,11 @@
+#!/bin/bash
 
-R -e "Sweave('deducorrect-vignette.Rnw')"
-pdflatex deducorrect-vignette.tex
-bibtex deducorrect-vignette
-pdflatex deducorrect-vignette.tex
-pdflatex deducorrect-vignette.tex
+doc=$1
+
+R -e "Sweave('deducorrect-${1}.Rnw')"
+pdflatex deducorrect-${1}.tex
+bibtex deducorrect-${1}
+pdflatex deducorrect-${1}.tex
+pdflatex deducorrect-${1}.tex
 
 
